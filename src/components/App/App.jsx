bcd5371eb910650bc89b1e02b9a749461a4d6586
Aleksei Usov <54838435+AlekseiUsov@users.noreply.github.com>
2023-07-24 10:31:25 +0300

    Revert "feat(small): adding styles mixins and variables"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,14 @@ import './App.scss';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const App = () => (
-	<div className="app__content">
-		<Header />
-		Start
-		<div className="app__test">Itim test</div>
-		<Footer />
-	</div>
-);
+function App() {
+	return (
+		<div className="app">
+			<Header />
+			Start
+			<Footer />
+		</div>
+	);
+}
 
 export default App;
