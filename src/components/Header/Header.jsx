5326b9f76cf1e7d174d57a4368ae61bf3a0adf63
Dmitry Litvinenko <ds-sev@ya.ru>
2023-07-27 18:23:24 +0300

    feat(small): create header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,23 @@
-import './Header.scss';
+import './Header.scss'
 
-const Header = () => <header className="header">Header</header>;
+const Header = () => (
+  <header className="header">
+    <input type="text" />
+    <div className='header__acc-info-container'>
+      <ul className='header__options-list options-list'>
+        <li className='option-list__item'>mail</li>
+        <li className='option-list__item'>bell</li>
+      </ul>
+      <div className='acc-container'>
+        <div className='acc-container__avatar' />
+        <div className='acc-container__info'>
+          <p className='acc-info__name'>Ольга Филимонова</p>
+          <p className='acc-info__job'>HR-менеджер</p>
+        </div>
+        <button>acc_menu</button>
+      </div>
+    </div>
+  </header>
+)
 
-export default Header;
+export default Header
