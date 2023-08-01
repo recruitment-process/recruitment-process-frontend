105ef4e1b2b089be65d5215c61ade54eedca4be3
Dmitry Litvinenko <ds-sev@ya.ru>
2023-08-01 12:13:58 +0300

    feat(small): add logo

--- src/components/UI/Checkbox/Checkbox.jsx
+++ src/components/UI/Checkbox/Checkbox.jsx
@@ -1,47 +1,56 @@
-import './Checkbox.scss'
-import PropTypes from 'prop-types'
+import './Checkbox.scss';
+import PropTypes from 'prop-types';
 
 export const Checkbox = ({ checked, onChange, label, disabled, ...props }) => (
-  <label htmlFor={props.id} className={`checkbox ${disabled && 'checkbox_disabled'} `}>
-    <input
-      type="checkbox"
-      className="checkbox__input"
-      id={props.id}
-      onChange={onChange}
-      defaultChecked={checked}
-    />
-    <span className={['checkbox__custom-cb', `${!disabled && (checked) ? 'checkbox__custom-cb_active' : ''}`, `${disabled && 'checkbox__custom-cb_disabled'}`].join(' ')} />
-    {label && <span className="checkbox__text">{label}</span>}
-  </label>
-)
+	<label
+		htmlFor={props.id}
+		className={`checkbox ${disabled && 'checkbox_disabled'} `}
+	>
+		<input
+			type="checkbox"
+			className="checkbox__input"
+			id={props.id}
+			onChange={onChange}
+			defaultChecked={checked}
+		/>
+		<span
+			className={[
+				'checkbox__custom-cb',
+				`${!disabled && checked ? 'checkbox__custom-cb_active' : ''}`,
+				`${disabled && 'checkbox__custom-cb_disabled'}`,
+			].join(' ')}
+		/>
+		{label && <span className="checkbox__text">{label}</span>}
+	</label>
+);
 
 Checkbox.propTypes = {
-  /**
-   * id for link label with checkbox
-   */
-  id: PropTypes.string,
-  /**
-   * is checkbox checked?
-   */
-  checked: PropTypes.bool,
-  /**
-   * text for label
-   */
-  label: PropTypes.string,
-  /**
-   * is checkbox disabled?
-   */
-  disabled: PropTypes.bool,
-  /**
-   * function in parent
-   */
-  onChange: PropTypes.func
-}
+	/**
+	 * id for link label with checkbox
+	 */
+	id: PropTypes.string,
+	/**
+	 * is checkbox checked?
+	 */
+	checked: PropTypes.bool,
+	/**
+	 * text for label
+	 */
+	label: PropTypes.string,
+	/**
+	 * is checkbox disabled?
+	 */
+	disabled: PropTypes.bool,
+	/**
+	 * function in parent
+	 */
+	onChange: PropTypes.func,
+};
 
 Checkbox.defaultProps = {
-  id: 'checkbox',
-  checked: false,
-  label: undefined,
-  disabled: false,
-  onChange: undefined
-}
+	id: 'checkbox',
+	checked: false,
+	label: undefined,
+	disabled: false,
+	onChange: undefined,
+};
