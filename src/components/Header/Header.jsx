188b2ae4cf649d5f35d04cef26303f8f16116514
Dmitry Litvinenko <ds-sev@ya.ru>
2023-08-01 12:12:09 +0300

    feat(small): add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,48 +1,67 @@
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
 // import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import './Header.scss'
-import InputWithError from '../UI/InputWithError/InputWithError'
-import Logo from '../UI/Logo/Logo'
-import avatar from '../../images/examples/avatar.png'
-
-
+import './Header.scss';
+import InputWithError from '../UI/InputWithError/InputWithError';
+import Logo from '../UI/Logo/Logo';
+import avatar from '../../images/examples/avatar.png';
 
 // import { useForm } from 'react-hook-form'
 
 const Header = () => {
-  const {control, handleSubmit, getValues} = useForm()
-  const onSubmit = () => {
-    handleSubmit(getValues)
-    // console.log(getValues)
-    console.log("data")
-  }
+	const { control, handleSubmit, getValues } = useForm();
+	const onSubmit = () => {
+		handleSubmit(getValues);
+		// console.log(getValues)
+		console.log('data');
+	};
 
-  return (
-    <header className="header">
-      <div className='header__logo-position'>
-      <Logo />
-      </div>
-      <form className='header__search-field' onSubmit={handleSubmit(onSubmit)}>
-         <InputWithError inputId='search' formName='searchForm' inputType='search' isDisabled={false} control={control} />
-      </form>
-      <div className='header__acc-info-container'>
-        <ul className='header__options-list options-list'>
-          <li><Link to="/" className='options-list__item options-list__item_mail' /></li>
-          <li><Link to="/" className='options-list__item options-list__item_notification' /></li>
-        </ul>
-        <div className='acc-container'>
-          <img className='acc-container__avatar' src={avatar} alt='Фото профиля'/>
-          <div className='acc-container__info'>
-            <p className='acc-info__name'>Ольга Филимонова</p>
-            <p className='acc-info__job'>HR-менеджер</p>
-          </div>
-          <button className='acc__menu'>.</button>
-        </div>
-      </div>
-    </header>
-  )
-}
+	return (
+		<header className="header">
+			<div className="header__logo-position">
+				<Logo />
+			</div>
+			<form className="header__search-field" onSubmit={handleSubmit(onSubmit)}>
+				<InputWithError
+					inputId="search"
+					formName="searchForm"
+					inputType="search"
+					placeholder="Поиск"
+					isDisabled={false}
+					control={control}
+				/>
+			</form>
+			<div className="header__acc-info-container">
+				<ul className="header__options-list options-list">
+					<li>
+						<Link
+							to="/"
+							className="options-list__item options-list__item_mail"
+						/>
+					</li>
+					<li>
+						<Link
+							to="/"
+							className="options-list__item options-list__item_notification"
+						/>
+					</li>
+				</ul>
+				<div className="acc-container">
+					<img
+						className="acc-container__avatar"
+						src={avatar}
+						alt="Фото профиля"
+					/>
+					<div className="acc-container__info">
+						<p className="acc-info__name">Ольга Филимонова</p>
+						<p className="acc-info__job">HR-менеджер</p>
+					</div>
+					<button className="acc__menu">.</button>
+				</div>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
