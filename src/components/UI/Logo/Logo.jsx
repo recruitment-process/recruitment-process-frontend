1545b6489097400b2a937cc12175bec0d9274b7d
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-08-02 17:33:36 +0700

    style(tiny): correct checkbox border color, correct padding input_type_search

--- src/components/UI/Logo/Logo.jsx
+++ src/components/UI/Logo/Logo.jsx
@@ -1,8 +1,6 @@
 import './Logo.scss';
-import logoPath from "../../../images/logo.svg";
+import logoPath from '../../../images/logo.svg';
 
-const Logo = () => (
-    <img className="logo" src={logoPath} alt="логотип" />
-);
+const Logo = () => <img className="logo" src={logoPath} alt="логотип" />;
 
 export default Logo;
