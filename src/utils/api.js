d68825ffd1a14b3f8d8e0107481a94813bc870af
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-09-09 01:08:33 +0700

    feat(tinny): add API integration

--- src/utils/api.js
+++ src/utils/api.js
@@ -23,3 +23,52 @@ export function authorize({ email, password }) {
 export function getUserInfo(userId) {
   return makeRequest(apiUrl, `/users/${userId}/`, 'GET');
 }
+
+// GET ALL VACANCIES
+export function getAllVacancies() {
+  return makeRequest(apiUrl, '/vacancies/', 'GET');
+}
+
+// GET VACANCY
+export function getVacancy(vacancyId) {
+  return makeRequest(apiUrl, `/vacancies/${vacancyId}`, 'GET');
+}
+
+// ADD VACANCY
+export function addVacancy({
+  vacancyTitle,
+  companyTitle,
+  website,
+  author,
+  requiredExperience,
+  employmentType,
+  scheduleWork,
+  salary,
+  city,
+  education,
+  pubDate,
+  jobConditions,
+  jobResponsibilities,
+  technologyStack,
+  vacancyStatus,
+  deadline,
+}) {
+  return makeRequest(apiUrl, '/vacancies/', 'POST', {
+    vacancy_title: vacancyTitle,
+    company_title: companyTitle,
+    website,
+    author,
+    required_experience: requiredExperience,
+    employment_type: employmentType,
+    schedule_work: scheduleWork,
+    salary,
+    city,
+    education,
+    pub_date: pubDate,
+    job_conditions: jobConditions,
+    job_responsibilities: jobResponsibilities,
+    technology_stack: technologyStack,
+    vacancy_status: vacancyStatus,
+    deadline,
+  });
+}
