3b974c276566b1adfd4cbeedef77f487552eea64
unknown <79168445519@yandex.ru>
2023-09-09 13:34:25 +0300

    feat(small): add main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,29 @@
-/* TODO Создан для эмуляции главной страницы, удалить когда она будет создана */
-const Main = () => <main className="main">Тут пока ничего нет</main>;
+import Widget from '../UI/Widget/Widget';
+import './Main.scss';
+
+import vacancyBtn from '../../temp/images/vacancy-btn.png';
+import MainVacancy from '../MainVacancy/MainVacancy';
+
+const Main = () => (
+  <section className="main-page">
+    <div className="main-page__main-container">
+      <div className="main-page__header-container">
+        <h3 className="main-page__header">Процесс найма</h3>
+        <img src={vacancyBtn} alt="кнопка Вакансия" />
+      </div>
+      <MainVacancy />
+      <h3 className="main-page__header">Срочные вакансии</h3>
+      <div className="main-page__urgent-vacancies" />
+    </div>
+    <div className="main-page__right-container">
+      <Widget>
+        <div className="analiz" />
+      </Widget>
+      <Widget>
+        <div className="interview" />
+      </Widget>
+    </div>
+  </section>
+);
 
 export default Main;
---
+++ src/components/MainVacancy/MainVacancy.jsx
@@ -0,0 +1,15 @@
+import './MainVacancy.scss';
+
+const MainVacancy = () => (
+  <section className="main-vacancy">
+    <div className="main-vacancy__header-container">
+      <h4 className="main-vacancy__header">Директор по маркетингу</h4>
+      <p className="main-vacancy__data">23.08.23</p>
+    </div>
+    <div className="main-vacancy__stage">
+      <div className="main-vacancy__stage-name"> Скрининг</div>
+    </div>
+  </section>
+);
+
+export default MainVacancy;
